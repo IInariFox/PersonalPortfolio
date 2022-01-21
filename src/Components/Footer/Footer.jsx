5b461331d14d7d3ea0da01c82c8d8baced46b139
nathon <nathonchavez01@gamil.com>
2022-01-21 11:01:51 +0900

    Finished footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import "./Footer.css";
+import {
+  AiFillGithub
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { GrResume } from "react-icons/gr";
+export const Footer = () => {
+  return (
+    <Row md={12} className="home-about-social">
+      <h1>FIND ME ON</h1>
+      <p>
+        Feel free to <span className="purple">connect </span>with me
+      </p>
+      <ul className="home-about-social-links">
+        <li className="social-icons">
+          <a
+            href="https://github.com/IInariFox"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour home-social-icons"
+          >
+            <AiFillGithub />
+          </a>
+        </li>
+        <li className="social-icons">
+          <a
+            href="https://www.linkedin.com/in/nathon-chavez-b8a958211/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour home-social-icons"
+          >
+            <FaLinkedinIn />
+          </a>
+        </li>
+      </ul>
+    </Row>
+  );
+};
